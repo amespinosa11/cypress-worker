1e3bdce96145158b1be6a81979fbe10c5290965d
amespinosa <aespinosa@puntosleal.com>
2020-04-04 18:06:19 -0500

    Arreglos para procesar pruebas

--- cypress.js
+++ cypress.js
@@ -1,7 +1,122 @@
 const shell = require('shelljs');
+const cypress = require('cypress')
+const CronJob = require('cron').CronJob;
+
+const SQS = require('./sqs/sqs');
+const sqs = new SQS();
+
+const fs = require('file-system');
+const axios = require('axios');
+
+const procesarPrueba = async() => {
+    cypress.run({
+        spec: './cypress/integration/login-habitica-web.spec.js',
+        browser: 'chrome',
+        headful: true
+      })
+      .then((results) => {
+        console.log('RES : ', results)
+      })
+      .catch((err) => {
+        console.error(err)
+      })
+}
+
+const desencolarMensajes = async(cola) => {
+    let a = await sqs.receiveMessage(cola);
+    //console.log('Desencolar mensaje ',a);
+    return a;
+}
 
 const startTest = ()=> {
+    shell.env['VAR_ANA'] = 'ana'
+    shell.exec('set');
     shell.exec('npm run cypress:run');
 }
 
-startTest();
+const job = new CronJob('0 55 17 * * *', async() => {
+    console.log('*** Vamos a procesar pruebas ***');
+    let prueba = await desencolarMensajes(``);
+    console.log('prueba : ', prueba);
+    if(prueba.code === 100) {
+        const pruebaEjecutar = {
+            idPrueba: parseInt(prueba.data.MessageAttributes.idPrueba.StringValue),
+            esScript: JSON.parse(prueba.data.MessageAttributes.esScript.StringValue),
+            scriptFile: prueba.data.MessageAttributes.scriptFile.StringValue,
+            modo: prueba.data.MessageAttributes.modo.StringValue,
+            navegador: prueba.data.MessageAttributes.navegador.StringValue,
+        };
+
+        console.log(pruebaEjecutar);
+        // Ahora se debe transferir el archivo a /integration
+        fs.copyFile(`../files/${pruebaEjecutar.scriptFile}`, `./cypress/integration/${pruebaEjecutar.scriptFile}`, (err) => {
+            if (err) throw err;
+          });
+
+        // Formar parametros prueba cypress
+        const setupCypress = {
+            spec: `./cypress/integration/${pruebaEjecutar.scriptFile}`,
+            browser: pruebaEjecutar.navegador,
+            headed: pruebaEjecutar.modo.toLowerCase() === 'headful' ? true : false,
+            headless: pruebaEjecutar.modo.toLowerCase() === 'headless' ? true : false
+        }
+
+        console.log('SETUP CYPRESS :', setupCypress);
+        let resultadosGuardar = {};
+        // Cambiar estado a en ejecucion
+        await axios.post('http://localhost:3000/estrategias/estado_prueba', {
+            idPrueba : pruebaEjecutar.idPrueba,
+            estado : 'enEjecucion'
+        });
+        // Correr prueba
+        cypress.run(setupCypress)
+          .then(async(results) => {
+                console.log('RESULTADOS : ', results);
+                resultadosGuardar.startedTestsAt = results.startedTestsAt;
+                resultadosGuardar.endedTestsAt = results.endedTestsAt;
+                resultadosGuardar.totalDuration = results.totalDuration;
+                resultadosGuardar.totalSuites = results.totalSuites;
+                resultadosGuardar.totalTests = results.totalTests;
+                resultadosGuardar.totalFailed = results.totalFailed;
+                resultadosGuardar.totalPassed = results.totalPassed;
+                resultadosGuardar.totalPending = results.totalPending;
+                resultadosGuardar.totalSkipped = results.totalSkipped;
+                resultadosGuardar.browserVersion = results.browserVersion;
+                resultadosGuardar.browserName = results.browserName;
+                console.log('RES : ', resultadosGuardar);
+                // Cambiar estado a satisfactorio o fallido
+                await axios.post('http://localhost:3000/estrategias/estado_prueba', {
+                    idPrueba : pruebaEjecutar.idPrueba,
+                    estado : resultadosGuardar.totalFailed === 0 ? 'satisfactoria' : 'fallida'
+                });
+                // Eliminar mensaje de la cola
+
+
+                // Guardar resultados en .json
+                fs.writeFile("../files/results/resultado.json", JSON.stringify(resultadosGuardar), 'utf8', (err) => {
+                    if (err) {
+                        console.log("An error occured while writing JSON Object to File.");
+                        return console.log(err);
+                    }
+                 
+                    console.log("JSON file has been saved.");
+                });
+                // Pasar archivos de resultados a otra carpeta
+
+                // Guardar resultado en tabla
+                
+          })
+          .catch(async(err) => {
+            await axios.post('http://localhost:3000/estrategias/estado_prueba', {
+                idPrueba : pruebaEjecutar.idPrueba,
+                estado : 'fallida'
+            });
+            console.error(err);
+          })
+        
+    }
+});
+
+job.start();
+
+//startTest();
---
+++ sqs/sqs.js
@@ -0,0 +1,131 @@
+const aws = require('aws-sdk');
+const q = require('q');
+
+// Load your AWS credentials and try to instantiate the object.
+aws.config = new aws.Config();
+aws.config.accessKeyId = process.env.ACCESS_KEY_ID;
+aws.config.secretAccessKey = process.env.SECRET_ACCESS_KEY;
+aws.config.region = process.env.AWS_REGION;
+aws.config.sessionToken = process.env.SESSION_TOKEN;
+
+// Instantiate SQS.
+const sqs = new aws.SQS();
+
+class SQS {
+    sendMessage = async(message,queueUrl) => {
+        let d = q.defer();
+        const params = {
+            MessageAttributes: {
+                "idPrueba": {
+                    DataType: "Number",
+                    StringValue: JSON.stringify(message.idPrueba)
+                },
+                "esScript": {
+                    DataType: "String",
+                    StringValue: JSON.stringify(message.esScript)
+                },
+                "scriptFile": {
+                    DataType: "String",
+                    StringValue: message.scriptFile
+                },
+                "tipo": {
+                    DataType: "String",
+                    StringValue: message.tipo
+                },
+                "herramienta": {
+                    DataType: "String",
+                    StringValue: message.herramienta
+                },
+                "modo": {
+                    DataType: "String",
+                    StringValue: message.modo
+                },
+                "navegador": {
+                    DataType: "String",
+                    StringValue: message.navegador
+                },
+                "resolucion": {
+                    DataType: "String",
+                    StringValue: message.resolucion
+                }
+            },
+            MessageBody: `${message.idPrueba}-${message.tipo}-${message.herramienta}-${message.modo}`,
+            QueueUrl: queueUrl,
+            DelaySeconds: 0
+        };
+        sqs.sendMessage(params, (err, data) => {
+            if(err) {
+                d.resolve({ code: 105, message: err });
+            }
+            else {
+                d.resolve({ code: 100, data: data });
+            }
+        });
+        return d.promise;
+    }
+
+    receiveMessage = async(queueUrl) => {
+        let d = q.defer();
+        const params = {
+            AttributeNames: [
+                "SentTimestamp"
+             ],
+             MaxNumberOfMessages: 1,
+             MessageAttributeNames: [
+                "All"
+             ],
+             QueueUrl: queueUrl,
+             VisibilityTimeout: 20,
+             WaitTimeSeconds: 0
+        };
+    
+        sqs.receiveMessage(params, (err, data) => {
+            if(err) {
+                d.resolve({ code: 105, message: err });
+            }
+            else {
+                let msg = data.Messages[0] !== undefined ? data.Messages[0] : [];
+                d.resolve({ code: 100, data: msg });
+            }
+        });
+        return d.promise;
+    }
+
+    getQueueMessages = async(queueUrl) => {
+        let d = q.defer();
+        const params = {
+            QueueUrl: queueUrl,
+            AttributeNames: [
+                "ApproximateNumberOfMessages"
+            ]
+        };
+        sqs.getQueueAttributes(params,(err, data) => {
+            if (err) {
+                d.resolve({ code: 105, message: err });
+            } else {
+                d.resolve({ code: 100, data: parseInt(data.Attributes.ApproximateNumberOfMessages) });
+            }
+        });
+        return d.promise;
+    }
+    
+    deleteMessage = async(queueUrl, receipt) => {
+        let d = q.defer();
+        const params = {
+            QueueUrl: queueUrl,
+            ReceiptHandle: receipt
+        };
+    
+        sqs.deleteMessage(params, function(err, data) {
+            if(err) {
+                d.resolve({ code: 105, message: err });
+            }
+            else {
+                d.resolve({ code: 100, data: 'Eliminado' });
+            }
+        });
+        return d.promise;
+    }
+}
+
+module.exports = SQS;
