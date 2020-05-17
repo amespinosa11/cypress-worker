24c0988e26d80d24611285b860c5aaafe15dc084
amespinosa <aespinosa@puntosleal.com>
2020-05-17 17:17:14 -0500

    Cambios para sprint 4

--- cypress.js
+++ cypress.js
@@ -28,93 +28,107 @@ const desencolarMensajes = async(cola) => {
     return a;
 }
 
+const obtenerCantidadMensajes = async(cola) => {
+    let a = await sqs.getQueueMessages(cola);
+    return a;
+}
+
+const eliminarMensaje = async(cola,receipt) => {
+    let a = await sqs.deleteMessage(cola,receipt);
+    console.log(a);
+}
+
 const startTest = ()=> {
     shell.env['VAR_ANA'] = 'ana'
     shell.exec('set');
     shell.exec('npm run cypress:run');
 }
 
-const job = new CronJob('0 55 17 * * *', async() => {
+const job = new CronJob('0 */1 * * * *', async() => {
     console.log('*** Vamos a procesar pruebas ***');
-    let prueba = await desencolarMensajes(``);
-    console.log('prueba : ', prueba);
-    if(prueba.code === 100) {
-        const pruebaEjecutar = {
-            idPrueba: parseInt(prueba.data.MessageAttributes.idPrueba.StringValue),
-            esScript: JSON.parse(prueba.data.MessageAttributes.esScript.StringValue),
-            scriptFile: prueba.data.MessageAttributes.scriptFile.StringValue,
-            modo: prueba.data.MessageAttributes.modo.StringValue,
-            navegador: prueba.data.MessageAttributes.navegador.StringValue,
-        };
+    let cantidadMensajes = await obtenerCantidadMensajes(`https://sqs.us-east-1.amazonaws.com/677094465990/Cypress`);
+    if(cantidadMensajes.data > 0) {
+        let prueba = await desencolarMensajes(`https://sqs.us-east-1.amazonaws.com/677094465990/Cypress`);
+        console.log('prueba : ', prueba);
+        if(prueba.code === 100) {
+            const pruebaEjecutar = {
+                idPrueba: parseInt(prueba.data.MessageAttributes.idPrueba.StringValue),
+                esScript: JSON.parse(prueba.data.MessageAttributes.esScript.StringValue),
+                scriptFile: prueba.data.MessageAttributes.scriptFile.StringValue,
+                modo: prueba.data.MessageAttributes.modo.StringValue,
+                navegador: prueba.data.MessageAttributes.navegador.StringValue,
+            };
 
-        console.log(pruebaEjecutar);
-        // Ahora se debe transferir el archivo a /integration
-        fs.copyFile(`../files/${pruebaEjecutar.scriptFile}`, `./cypress/integration/${pruebaEjecutar.scriptFile}`, (err) => {
-            if (err) throw err;
-          });
+            console.log(pruebaEjecutar);
+            // Ahora se debe transferir el archivo a /integration
+            fs.copyFile(`../files/${pruebaEjecutar.scriptFile}`, `./cypress/integration/${pruebaEjecutar.scriptFile}`, (err) => {
+                if (err) throw err;
+            });
 
-        // Formar parametros prueba cypress
-        const setupCypress = {
-            spec: `./cypress/integration/${pruebaEjecutar.scriptFile}`,
-            browser: pruebaEjecutar.navegador,
-            headed: pruebaEjecutar.modo.toLowerCase() === 'headful' ? true : false,
-            headless: pruebaEjecutar.modo.toLowerCase() === 'headless' ? true : false
-        }
+            // Formar parametros prueba cypress
+            const setupCypress = {
+                spec: `./cypress/integration/${pruebaEjecutar.scriptFile}`,
+                browser: pruebaEjecutar.navegador.toLowerCase(),
+                headed: pruebaEjecutar.modo.toLowerCase() === 'headful' ? true : false,
+                headless: pruebaEjecutar.modo.toLowerCase() === 'headless' ? true : false
+            }
 
-        console.log('SETUP CYPRESS :', setupCypress);
-        let resultadosGuardar = {};
-        // Cambiar estado a en ejecucion
-        await axios.post('http://localhost:3000/estrategias/estado_prueba', {
-            idPrueba : pruebaEjecutar.idPrueba,
-            estado : 'enEjecucion'
-        });
-        // Correr prueba
-        cypress.run(setupCypress)
-          .then(async(results) => {
-                console.log('RESULTADOS : ', results);
-                resultadosGuardar.startedTestsAt = results.startedTestsAt;
-                resultadosGuardar.endedTestsAt = results.endedTestsAt;
-                resultadosGuardar.totalDuration = results.totalDuration;
-                resultadosGuardar.totalSuites = results.totalSuites;
-                resultadosGuardar.totalTests = results.totalTests;
-                resultadosGuardar.totalFailed = results.totalFailed;
-                resultadosGuardar.totalPassed = results.totalPassed;
-                resultadosGuardar.totalPending = results.totalPending;
-                resultadosGuardar.totalSkipped = results.totalSkipped;
-                resultadosGuardar.browserVersion = results.browserVersion;
-                resultadosGuardar.browserName = results.browserName;
-                console.log('RES : ', resultadosGuardar);
-                // Cambiar estado a satisfactorio o fallido
-                await axios.post('http://localhost:3000/estrategias/estado_prueba', {
-                    idPrueba : pruebaEjecutar.idPrueba,
-                    estado : resultadosGuardar.totalFailed === 0 ? 'satisfactoria' : 'fallida'
-                });
-                // Eliminar mensaje de la cola
-
-
-                // Guardar resultados en .json
-                fs.writeFile("../files/results/resultado.json", JSON.stringify(resultadosGuardar), 'utf8', (err) => {
-                    if (err) {
-                        console.log("An error occured while writing JSON Object to File.");
-                        return console.log(err);
-                    }
-                 
-                    console.log("JSON file has been saved.");
-                });
-                // Pasar archivos de resultados a otra carpeta
-
-                // Guardar resultado en tabla
-                
-          })
-          .catch(async(err) => {
+            console.log('SETUP CYPRESS :', setupCypress);
+            let resultadosGuardar = {};
+            // Cambiar estado a en ejecucion
             await axios.post('http://localhost:3000/estrategias/estado_prueba', {
                 idPrueba : pruebaEjecutar.idPrueba,
-                estado : 'fallida'
+                estado : 'EN_EJECUCION'
             });
-            console.error(err);
-          })
-        
+            // Correr prueba
+            cypress.run(setupCypress)
+            .then(async(results) => {
+                    console.log('RESULTADOS : ', results);
+                    resultadosGuardar.startedTestsAt = results.startedTestsAt;
+                    resultadosGuardar.endedTestsAt = results.endedTestsAt;
+                    resultadosGuardar.totalDuration = results.totalDuration;
+                    resultadosGuardar.totalSuites = results.totalSuites;
+                    resultadosGuardar.totalTests = results.totalTests;
+                    resultadosGuardar.totalFailed = results.totalFailed;
+                    resultadosGuardar.totalPassed = results.totalPassed;
+                    resultadosGuardar.totalPending = results.totalPending;
+                    resultadosGuardar.totalSkipped = results.totalSkipped;
+                    resultadosGuardar.browserVersion = results.browserVersion;
+                    resultadosGuardar.browserName = results.browserName;
+                    console.log('RES : ', resultadosGuardar);
+                    // Cambiar estado a satisfactorio o fallido
+                    await axios.post('http://localhost:3000/estrategias/estado_prueba', {
+                        idPrueba : pruebaEjecutar.idPrueba,
+                        estado : resultadosGuardar.totalFailed === 0 ? 'SATISFACTORIA' : 'FALLIDA'
+                    });
+                    // Eliminar mensaje de la cola
+                    await eliminarMensaje(`https://sqs.us-east-1.amazonaws.com/677094465990/Cypress`, prueba.data.ReceiptHandle)
+
+                    // Guardar resultados en .json
+                    fs.writeFile("../files/results/resultado.json", JSON.stringify(resultadosGuardar), 'utf8', (err) => {
+                        if (err) {
+                            console.log("An error occured while writing JSON Object to File.");
+                            return console.log(err);
+                        }
+                    
+                        console.log("JSON file has been saved.");
+                    });
+                    // Pasar archivos de resultados a otra carpeta
+
+                    // Guardar resultado en tabla
+                    
+            })
+            .catch(async(err) => {
+                await axios.post('http://localhost:3000/estrategias/estado_prueba', {
+                    idPrueba : pruebaEjecutar.idPrueba,
+                    estado : 'FALLIDA'
+                });
+                console.error(err);
+            })
+            
+        }
     }
+    
 });
 
 job.start();
